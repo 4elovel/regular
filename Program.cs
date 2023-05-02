b7e7b2fee74b7a9aa63870a707318d62f56a5bbd
4elovel <marcynakvova007@gmail.com>
2023-05-02 18:24:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("введіть текст:");
+        string checker = @"(.*?\d.*?\d.*?\d[a-z]*?)";
+        Regex reg = new Regex(checker);
+        string buf = Console.ReadLine();
+        if (buf.Length==9) {
+            Console.WriteLine(reg.IsMatch(buf));
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+}
